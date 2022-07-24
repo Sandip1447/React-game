b5446f842617e1364f009c71b99ea4a5433c1872
Sandip Rode <16761010+Sandip1447@users.noreply.github.com>
2022-07-24 13:07:42 +0530

    Adding Image Background

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, ImageBackground } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import StartGame from "./screens/StartGame";
 
@@ -8,14 +8,23 @@ export default function App() {
       style={styles.rootContainer}
       colors={["#CA4E79", "#FFC18E"]}
     >
-      <StartGame />
+      <ImageBackground
+        source={require("./assets/fox_paint_artwork_5k-720x1280.jpg")}
+        resizeMode="cover"
+        style={styles.rootContainer}
+        imageStyle={styles.backgroundImage}
+      >
+        <StartGame />
+      </ImageBackground>
     </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   rootContainer: {
-    backgroundColor: "#FFC18E",
     flex: 1,
+  },
+  backgroundImage: {
+    opacity: 0.15,
   },
 });
